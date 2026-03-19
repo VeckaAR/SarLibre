cf545163b8f9ea36bd70994399a83ca4f2070bcb
Vero Abrego <vero.abrego29@gmail.com>
2026-03-19 13:08:08 -0600

    Mejora visual del módulo de horas libres

--- SARinfo/view/footer.php
+++ SARinfo/view/footer.php
@@ -1,5 +1,5 @@
 <footer class="col-lg-12 col-md-12 col-sm-12 col-xs-12 justify-content-center">
-        <p class="legal justify-content-center">© Copyright 2023 | Universidad Don Bosco | All Rights Reserved.</p>
+        <p class="legal justify-content-center">© Copyright 2026 | Universidad Don Bosco | All Rights Reserved.</p>
         <p class="credit">Desarrollo: Verónica Abrego</p>
     </footer>
 <script>
--- index.php
+++ index.php
@@ -8,22 +8,24 @@ if(!isset($param_sala))
 $salas = array();
 $query_salas = mysqli_query($connect, "SELECT idsala FROM salas ORDER BY idsala ASC");
 while ($sala = mysqli_fetch_assoc($query_salas)) {
-	// la primera es el id, la segunda es la cantidad actual, la tercera es si esta lleno o no
 	array_push($salas, array($sala["idsala"], 0, false, 0));
 }
 
 $ocupados = array();
-$query_reservas = mysqli_query($connect, "SELECT count( * ) AS cantidad, R.idsala, S.capacidad FROM reserva R INNER JOIN salas S on S.idsala = R.idsala WHERE R.hora_salida = 0 OR R.hora_salida IS NULL GROUP BY R.idsala, S.capacidad");
+$query_reservas = mysqli_query($connect, "SELECT count(*) AS cantidad, R.idsala, S.capacidad FROM reserva R INNER JOIN salas S on S.idsala = R.idsala WHERE R.hora_salida = 0 OR R.hora_salida IS NULL GROUP BY R.idsala, S.capacidad");
 while ($ocupado = mysqli_fetch_assoc($query_reservas)) {
 	array_push($ocupados, array($ocupado["idsala"], $ocupado["cantidad"], $ocupado["capacidad"]));
 }
 
 foreach ($ocupados as $ocupado) {
 	$key = array_search(array($ocupado[0], 0, false, 0), $salas);
-	$salas[$key][1] = $ocupado[1];
-	$salas[$key][3] = $ocupado[2];
-	if ($ocupado[1] >= $ocupado[2])
-		$salas[$key][2] = true;
+	if ($key !== false) {
+		$salas[$key][1] = $ocupado[1];
+		$salas[$key][3] = $ocupado[2];
+		if ($ocupado[1] >= $ocupado[2]) {
+			$salas[$key][2] = true;
+		}
+	}
 }
 
 $query_capacidades = mysqli_query($connect, "SELECT capacidad FROM salas ORDER BY idsala ASC");
@@ -32,108 +34,93 @@ while ($capacidad = mysqli_fetch_assoc($query_capacidades)) {
 	$salas[$i][3] = $capacidad["capacidad"];
 	$i = $i+1;
 }
-
 ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
 	<meta charset="UTF-8" />
-	<title>Document</title>
 	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
-	<title>MiniSAR</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>Laboratorio de Informática | Módulo de horas libres</title>
 	<link rel="stylesheet" href="bootstrap.css" />
-	<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
-	<script   src="https://code.jquery.com/jquery-2.2.4.js"   integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="   crossorigin="anonymous"></script>
-	<script type="text/javascript">
-	$(document).ready(function(){
-		$(document).on("click",".aConsultar",function(e){
-			console.log("hola");
-			window.location.href = "matenimiento.php"+"?carnet="+$("#idcarnet").val()+""; //Envío carnet
-		});
-	})
-	</script>
-	<?php
-/*foreach ($variable as $key => $value) {
-	# code...
-}*/
-
-?>
+	<link rel="stylesheet" href="home.css" />
+	<script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
 </head>
-<body onload="form1.carnet.focus()">
-	<header>
-		<div class="title">
-			<h2>Laboratorio de Informática</h2>
-			<h4>MÓDULO DE HORAS LIBRES</h4>
-		</div>
-		<div class="logo">
-			<img src="img/logo.png" alt="logo UDB" />
+<body onload="document.form1.carnet.focus()">
+	<header class="home-header">
+		<div class="home-header-inner">
+			<div class="">
+				<h1>Laboratorio de Informática</h1>
+				<p>Módulo de horas libres</p>
+			</div>
+			<div class="home-logo">
+				<img src="img/logo.png" alt="Logo UDB" />
+			</div>
 		</div>
 	</header>
-	<section class="contenedor">
-		<div id="form01">
-			<img src="img/fondo.jpg" alt="" class="fondo">
-			<form class="form-inline" action="wsinfo.php" method="post" name="form1" id="form1">
-				<div class="form">
-					<h2>BIENVENIDO</h2>
-					<div class="">
-						<label for="Carnet" class="lbcarnet lb">Carnet: </label>					
-						<input class="ipcarnet"type="text" id="idcarnet" name="carnet" required autofocus>
+
+	<main class="home-main">
+		<section class="hero-card">
+			<div class="hero-overlay"></div>
+
+			<div class="hero-left">
+				<form action="wsinfo.php" method="post" name="form1" id="form1" class="access-card">
+					<h2>Bienvenido</h2>
+					<p>Realiza tu reserva de forma rápida y segura.</p>
+
+					<div class="field">
+						<label for="idcarnet">Carnet</label>
+						<input type="text" id="idcarnet" name="carnet" required autofocus>
 					</div>
-					<div class="select">
-						<label for="Sala" class="lb" >Sala:&nbsp;&nbsp;&nbsp; </label>
+
+					<div class="field">
+						<label for="salaSelect">Sala</label>
 						<select name="sala" id="salaSelect">
 							<?php foreach($salas as $sala): ?>
-								<option data-max="<?=$sala[3]?>" data-cant=<?=$sala[1]?> <?=$param_sala == $sala[0] ? "selected" : ""?> value="<?=$sala[0]?>">
-									<?=$sala[0]?>
-								</option>	
+								<option
+									data-max="<?=$sala[3]?>"
+									data-cant="<?=$sala[1]?>"
+									<?=$param_sala == $sala[0] ? "selected" : ""?>
+									value="<?=$sala[0]?>">
+									Sala <?=$sala[0]?>
+								</option>
 							<?php endforeach; ?>
 						</select>
 					</div>
-					<div class="botones">
-						<a class="boton btn" href="SARinfo/menuInfo.php">Consultar</a>
-						<a href="cortehoras.php" class="boton btn" >Realizar corte</a>
-						<input type="submit" value="Reservar" class="btn btncorte"> 
+
+					<div class="actions">
+						<a class="home-btn secondary" href="SARinfo/menuInfo.php">Consultar</a>
+						<a class="home-btn secondary" href="cortehoras.php">Realizar corte</a>
+						<input type="submit" value="Reservar" class="home-btn primary">
 					</div>
-				</div>
-
-			</form>
-			<p> 
-				<form id="frm" action="consulta.php">
-					<!-- class="btn btn-warning" href="consulta.php"></a> -->
-					<input type="hidden" name="txtCarnet" value="">
 				</form>
-			</p>
-		</div>
-
-		<span class="Estilo7">ESTUDIANTES EN HORAS LIBRES </span>
-		<div id="wrapper" class="wrapper">
-			<div id="page">
-				<table class="tbstilo">
-					<tr>
-						<?php foreach($salas as $sala): ?>
-							<td><div align="center">Sala <?=$sala[0]?></div></td>	
-						<?php endforeach; ?>
-		
-					</tr>
-					<tr class="trborder">
-						<?php foreach($salas as $sala): ?>
-							<td class="<?=$sala[2] == true ? "bg-red" : ""?>">
-								<div align="center"><?=$sala[1]?></div>
-							</td>	
-						<?php endforeach; ?>
-					</tr>
-				</table>
 			</div>
-		</div>
-	</section><!-- end page -->
-	<footer class="col-lg-12 col-md-12 col-sm-12 col-xs-12 justify-content-center">
-        <p class="legal justify-content-center">© Copyright 2026 | Universidad Don Bosco | All Rights Reserved.</p>
-        <p class="credit">Desarrollo: Verónica Abrego</p>
+
+			<div class="hero-right"></div>
+		</section>
+
+		<section class="status-section">
+			<div class="status-title">Estudiantes en horas libres</div>
+			<p class="status-subtitle">Disponibilidad actual por laboratorio</p>
+
+			<div class="rooms-grid">
+				<?php foreach($salas as $sala): ?>
+					<div class="room-box <?=$sala[2] == true ? "full" : ""?>">
+						<div class="room-name">Sala <?=$sala[0]?></div>
+						<div class="room-count"><?=$sala[1]?></div>
+						<div class="room-capacity">Capacidad: <?=$sala[3]?></div>
+					</div>
+				<?php endforeach; ?>
+			</div>
+		</section>
+	</main>
+
+	<footer class="home-footer">
+        <p>© Copyright 2026 | Universidad Don Bosco | All Rights Reserved.</p>
+        <p>Desarrollo: Verónica Abrego</p>
     </footer>
 
-</body>
-<script language="JavaScript">
+<script>
 document.form1.carnet.focus();
 
 let form = document.querySelector("#form1");
@@ -146,6 +133,6 @@ form.addEventListener("submit", (e) => {
 	// 	e.preventDefault();
 	// }
 });
-
 </script>
+</body>
 </html>
